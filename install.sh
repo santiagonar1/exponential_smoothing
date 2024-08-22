0f6439ab7ffbae320c15b3c5704e259ad7d58bbb
Santiago Narvaez <santiagonar1@gmail.com>
2024-08-22 17:12:48 +0200

    scripts: add install script

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+BUILD_DIR=build-tmp
+
+# Function to prompt the user for yes or no
+prompt_user() {
+    read -p "The folder ${BUILD_DIR} already exists and WILL BE DELETED. Do you want to continue? (yes/no): " yn
+    case $yn in
+        [Yy]* )
+              echo "Proceeding..."
+              return
+              ;;
+          * )
+              echo "Aborting..."
+              exit 1
+              ;;
+    esac
+}
+
+if [ -d "${BUILD_DIR}" ]; then
+  prompt_user
+fi
+
+CMAKE_FLAGS="-DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=conan_provider.cmake -DBUILD_TESTING=OFF"
+
+
+rm -rf ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+cmake ${CMAKE_FLAGS} -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build . --target install
+
+cd ..
+rm -rf ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+cmake ${CMAKE_FLAGS} -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --target install
+cd ..
+rm -rf ${BUILD_DIR}
