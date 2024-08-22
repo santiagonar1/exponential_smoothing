08220cd7c22b6cfc021bd4668a2055c40ad894a1
Santiago Narvaez <santiagonar1@gmail.com>
2024-08-22 15:54:16 +0200

    scripts: compare results with cpp forecast

--- scripts/forecast_comparison.py
+++ scripts/forecast_comparison.py
@@ -18,6 +18,7 @@ def main():
 
     training = np.loadtxt("training_data.txt")
     real_forecast = np.loadtxt("real_forecast_data.txt")
+    y_cpp_forecast = np.loadtxt("cpp_forecast_data.txt")
 
     x_training = training[:, 0]
     y_training = training[:, 1]
@@ -28,16 +29,27 @@ def main():
     model = ExponentialSmoothing(y_training, trend="additive").fit(optimized=True)
     y_forecast = model.forecast(num_forecasts)
 
-    plt.figure()  # optional: initializes a new figure
+    print("Parameters values:")
+    print(model.params)
+
+    plt.figure(1)  # optional: initializes a new figure
 
     plt.plot(x_training, y_training, 'x', label='Training data')
     plt.plot(x_real_forecast[:num_forecasts], y_forecast, 'r', label='Forecast')
+    plt.plot(x_real_forecast[:len(y_cpp_forecast)], y_cpp_forecast, label='C++ forecast')
     plt.plot(x_real_forecast, y_real_forecast, 'g', label='Real data')
 
     plt.title('Model output')
     plt.xlabel('X values')
     plt.ylabel('Y values')
     plt.legend()
+
+    plt.show()
+
+    plt.figure(2)
+
+    plt.plot(abs(y_cpp_forecast - y_forecast))
+    plt.title('Error: CPP values - python values')
 
     plt.show()
 
